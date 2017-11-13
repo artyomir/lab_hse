09c48bdaa33064ab1e806922172cf227cbfa2451
Artyom Mirabyan <aamirabyan@edu.hse.ru>
2017-11-13 09:11:22 +0300

    For checking

--- main.cpp
+++ main.cpp
@@ -10,19 +10,19 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    Dish * water = new Dish("ASHOT",10);
-    Dish * fire = new Dish("ARTYOM",20);
+    Dish * water = new Dish("Paste",10);
+    Dish * fire = new Dish("Potate",20);
 
     Tree_RB<Dish*> tree(water, &cmp);
-    std::cout << "THE END";
-    tree.saveToJSFile("C:/Users/Artyom Mirabyan/Desktop/qwerty.txt", &Dish_inJSFile);
-    tree.loadFromJSFile("C:/Users/Artyom Mirabyan/Desktop/qwerty.txt", &Dish_fromJSFile);
+
+    tree.saveToJSFile("take filename here", &Dish_inJSFile);
+    tree.loadFromJSFile("take filename here", &Dish_fromJSFile);
 
     Tree_RB<Dish *> t = tree;
 
     tree.remove(water);
     t.insert(fire);
-    tree.saveToJSFile("C:/Users/Artyom Mirabyan/Desktop/TREE1.txt", &Dish_inJSFile);
-    t.saveToJSFile("C:/Users/Artyom Mirabyan/Desktop/TREE2.txt", &Dish_inJSFile);
+    tree.saveToJSFile("take filename here", &Dish_inJSFile);
+    t.saveToJSFile("take filename here", &Dish_inJSFile);
     return a.exec();
 }
